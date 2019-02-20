dd41ae4a437af7722f3149b59929560df53d1915
R Richards <rdrrichards@hotmail.com>
2019-02-19 19:06:38 -0600

    css... why so frustrating

--- src/app/shell/top-bar/top-bar.component.ts
+++ src/app/shell/top-bar/top-bar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
-
+  appTitle = 'This is the name of the application';
   constructor() { }
 
   ngOnInit() {

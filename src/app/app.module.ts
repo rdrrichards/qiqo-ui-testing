3f9eeca7ae439f9edad9ea018f47db0e1b814b59
R. Richards <rdrrichards@hotmail.com>
2019-04-11 18:53:55 -0500

    sql editor testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,20 @@ import { ShellModule } from './shell/shell.module';
 import { AppErrorHandler } from './shared/error-handler';
 import { MessageService } from 'primeng/api';
 import { SharedModule } from './shared/shared.module';
+import { SqlEditorComponent } from './sql-editor/sql-editor.component';
+import { AceEditorModule } from 'ng2-ace-editor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SqlEditorComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ShellModule,
-    SharedModule
+    SharedModule,
+    AceEditorModule
   ],
   providers: [ MessageService, { provide: ErrorHandler, useClass: AppErrorHandler, deps: [ MessageService ] } ],
   bootstrap: [AppComponent]

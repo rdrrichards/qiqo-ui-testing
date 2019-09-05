3377858472119de4290c72d5b9ba3f9eb8bca812
R. Richards <rdrrichards@hotmail.com>
2019-09-04 19:26:02 -0500

    cli update to latest

--- src/app/shared/error-handler.ts
+++ src/app/shared/error-handler.ts
@@ -1,6 +1,7 @@
-import { ErrorHandler } from '@angular/core';
+import { ErrorHandler, Injectable } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
+@Injectable()
 export class AppErrorHandler implements ErrorHandler {
   constructor(private messageService: MessageService) {}
   handleError(error: any) {

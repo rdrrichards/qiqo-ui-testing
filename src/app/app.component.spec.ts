179f1505ac4c64b93e763365acbd9eabae8f5ed8
R. Richards <rdrrichards@hotmail.com>
2019-12-03 13:00:02 -0600

    jasmine update to latest commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'primeng-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('primeng-test');
-  });
 });

fc883e71acb8034b9e4c33f53da454e483f6dc9a
R. Richards <rdrrichards@hotmail.com>
2020-01-12 13:05:11 -0600

    ditched sql editor thing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './shell/top-bar/top-bar.component';
-import { SqlEditorComponent } from './sql-editor/sql-editor.component';
 import { SharedModule } from './shared/shared.module';
-import { AceEditorModule } from 'ng2-ace-editor';
 import { MessageService } from 'primeng/api';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent, TopBarComponent, SqlEditorComponent,
+        AppComponent, TopBarComponent
       ],
-      imports: [SharedModule, AceEditorModule],
+      imports: [SharedModule],
       providers: [MessageService]
     }).compileComponents();
   }));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,11 @@ import { ShellModule } from './shell/shell.module';
 import { AppErrorHandler } from './shared/error-handler';
 import { MessageService } from 'primeng/api';
 import { SharedModule } from './shared/shared.module';
-import { SqlEditorComponent } from './sql-editor/sql-editor.component';
-import { AceEditorModule } from 'ng2-ace-editor';
 import { SelectTestingComponent } from './select-testing/select-testing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SqlEditorComponent,
     SelectTestingComponent
   ],
   imports: [
@@ -23,8 +20,7 @@ import { SelectTestingComponent } from './select-testing/select-testing.componen
     BrowserAnimationsModule,
     ReactiveFormsModule,
     ShellModule,
-    SharedModule,
-    AceEditorModule
+    SharedModule
   ],
   providers: [ MessageService, { provide: ErrorHandler, useClass: AppErrorHandler, deps: [ MessageService ] } ],
   bootstrap: [AppComponent]

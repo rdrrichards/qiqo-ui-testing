22cc3bcaab2e86063f1f34fab8c8da399aabe383
R. Richards <rdrrichards@hotmail.com>
2020-04-30 18:20:37 -0500

    vertical toolbars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,27 +10,26 @@ import { ContextMenu } from 'primeng/contextmenu';
 export class AppComponent {
   title = 'primeng-test';
   items: MenuItem[];
+  utils: MenuItem[];
   displayAbout = false;
   // @ViewChild(ContextMenu, { static: true }) appCxMenu: ContextMenu;
   constructor(private messageService: MessageService) {
     this.items = [
-      {
-        label: 'Recent',
-        icon: 'pi pi-fw pi-clock'
-      },
-      {
-        label: 'About', command: (event) => { this.displayAbout = true; }
-      },
-      {
-        separator: true,
-      },
-      {
-        label: 'Logout',
-      }
-  ];
+      { label: 'Sign', icon: 'pi pi-pencil', command: (event) => { console.log('Sign', event); }},
+      { label: 'Image', icon: 'pi pi-image', command: (event) => { console.log('Image', event); }},
+      { label: 'Changes', icon: 'pi pi-money-bill', command: (event) => { console.log('Changes', event); }},
+      { label: 'Camera', icon: 'pi pi-camera', command: (event) => { console.log('Camera', event); }}
+    ];
+    this.utils = [
+      { label: 'Refresh', icon: 'pi pi-refresh', command: (event) => { console.log('Refresh', event); }},
+      { label: 'Undo', icon: 'pi pi-undo', command: (event) => { console.log('Undo', event); }},
+      { label: 'Redo', icon: 'pi pi-replay', command: (event) => { console.log('Redo', event); }},
+      { label: 'Zoom In', icon: 'pi pi-search-plus', command: (event) => { console.log('Zoom In', event); }},
+      { label: 'Zoom Out', icon: 'pi pi-search-minus', command: (event) => { console.log('Zoom In', event); }}
+    ];
   }
   // showAbout() {
-  //   this.displayAbout = true;
+  //   console.log('');
   // }
   onConfirm() {
     this.messageService.clear('c');

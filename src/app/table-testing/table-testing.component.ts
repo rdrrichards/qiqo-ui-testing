d2cedefbd4dfc18064ddccdaeccc5a2a80091ce8
R. Richards <rdrrichards@hotmail.com>
2025-01-20 11:54:24 -0600

    table testing to find repro

--- src/app/table-testing/table-testing.component.ts
+++ src/app/table-testing/table-testing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
     selector: 'app-table-testing',
@@ -7,17 +8,13 @@ import { Component, OnInit } from '@angular/core';
     standalone: false
 })
 export class TableTestingComponent implements OnInit {
-  cars: any[];
+  cars: Observable<any[]>;
   cols: any[];
-
-  constructor() { }
-
+  selectedResults: any[] = [];
+  trackByFunction = (index: number, item: any) => item.vin;
   ngOnInit() {
     // this.carService.getCarsSmall().then(cars => this.cars = cars);
-    this.cars = [
-      { status: 'pi-thumbs-up', statusColor: 'red', vin: '938429huhed98ujq3', year: '2017', brand: 'Acura', color: 'Silver' },
-      { status: 'pi-check-circle', statusColor: 'blue', vin: 'huhed98ujq3938429', year: '2020', brand: 'Toyota', color: 'Blue' }
-    ];
+    this.lodaData();
     this.cols = [
         { field: 'status', header: 'Status' },
         { field: 'vin', header: 'Vin' },
@@ -25,5 +22,33 @@ export class TableTestingComponent implements OnInit {
         { field: 'brand', header: 'Brand' },
         { field: 'color', header: 'Color' }
     ];
+  }
+  sortTable(event: any) {
+    console.log(event);
+    this.lodaData();
+  }
+  headerCheckboxToggled(event: any) {
+    // this.headerCheckboxToggle.emit(event.checked);
+    // this.buildContextMenu();
+  }
+  rowSelected(event: any) {
+    event.originalEvent.stopPropagation();
+    // this.rowSelect.emit(this.selectedResults.length > 0);
+    // this.buildContextMenu();
+  }
+  rowUnselected(event: any) {
+    // this.rowUnselect.emit(this.selectedResults.length > 0);
+    // this.buildContextMenu();
+  }
+  lodaData() {
+    this.cars = of([
+      { status: 'pi-thumbs-up', statusColor: 'red', vin: '938429huhed98ujq3', year: '2017', brand: 'Acura', color: 'Silver' },
+      { status: 'pi-check-circle', statusColor: 'blue', vin: 'huhed98ujq3938429', year: '2020', brand: 'Toyota', color: 'Blue' },
+      { status: 'pi-times-circle', statusColor: 'green', vin: '98ujq3938429huhed', year: '2019', brand: 'Honda', color: 'Black' },
+      { status: 'pi-check-circle', statusColor: 'orange', vin: '3huhed98ujq393842', year: '2018', brand: 'Nissan', color: 'Orange' },
+      { status: 'pi-times-circle', statusColor: 'purple', vin: 'hed98ujq3938429hu', year: '2017', brand: 'Renault', color: 'Gray' },
+      { status: 'pi-check-circle', statusColor: 'teal', vin: 'uhed98ujq3938429h', year: '2016', brand: 'Volkswagen', color: 'Silver' },
+      { status: 'pi-times-circle', statusColor: 'violet', vin: '98ujq3938429huhe', year: '2015', brand: 'Volvo', color: 'White' }
+    ]);
   }
 }
